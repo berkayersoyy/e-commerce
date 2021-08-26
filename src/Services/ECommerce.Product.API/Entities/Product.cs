356132eec99ab61fb492480955a25296c6a6457e
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-26 23:57:20 +0300

    Entity added

---
+++ src/Services/ECommerce.Product.API/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace ECommerce.Product.API.Entities
+{
+    public class Product
+    {
+        [BsonId] public string Id { get; set; }
+        [BsonElement("Name")] public string Name { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+    }
+}
