4824415f344b9c32589be8f3b07b1fa604ea72ad
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-28 00:22:36 +0300

    RabbitMQ added

---
+++ src/Services/Common/EventBusRabbitMQ/DefaultRabbitMQPersistentConnection.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Net.Sockets;
+using RabbitMQ.Client;
+using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Retry;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+
+namespace EventBusRabbitMQ
+{
+    public class DefaultRabbitMQPersistentConnection:IRabbitMQPersistentConnection
+    {
+        private readonly IConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private readonly int _retryCount;
+        private readonly ILogger<DefaultRabbitMQPersistentConnection> _logger;
+        private bool _disposed;
+
+        public DefaultRabbitMQPersistentConnection(IConnectionFactory connectionFactory, int retryCount, ILogger<DefaultRabbitMQPersistentConnection> logger)
+        {
+            _connectionFactory = connectionFactory;
+            _retryCount = retryCount;
+            _logger = logger;
+        }
+
+        public bool IsConnected
+        {
+            get
+            {
+                return _connection != null && _connection.IsOpen && !_disposed;
+            }
+        }
+
+        public bool TryConnect()
+        {
+            _logger.LogInformation("RabbitMQ Client is trying to connect");
+
+            var policy = RetryPolicy.Handle<SocketException>().Or<BrokerUnreachableException>()
+                .WaitAndRetry(_retryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    (ex, time) =>
+                    {
+                        _logger.LogWarning(ex,"RabbitMQ Client could not connect after {TimeOut}s ({ExceptionMessage})",$"{time.TotalSeconds:N1}",ex.Message);
+                    });
+            policy.Execute(() =>
+            {
+                _connection = _connectionFactory.CreateConnection();
+            });
+            if (IsConnected)
+            {
+                _connection.ConnectionShutdown += OnConnectionShutdown;
+                _connection.CallbackException += OnCallbackException;
+                _connection.ConnectionBlocked += OnConnectionBlocked;
+
+                _logger.LogInformation("RabbitMQ Client acquired a persistent connection to '{HostName}' and is subscribed to failure events",_connection.Endpoint.HostName);
+                return true;
+            }
+            else
+            {
+                _logger.LogCritical("FATAL ERROR: RabbitMQ connection could not be created or opened");
+                return false;
+            }
+        }
+
+        private void OnConnectionBlocked(object sender, ConnectionBlockedEventArgs e)
+        {
+            if (_disposed) return;
+
+            _logger.LogWarning("A RabbitMQ connection is shutdown. Trying to re-connect...");
+
+            TryConnect();
+        }
+
+        private void OnCallbackException(object sender, CallbackExceptionEventArgs e)
+        {
+            if (_disposed) return;
+
+            _logger.LogWarning("A RabbitMQ connection throw exception. Trying to re-connect...");
+
+            TryConnect();
+        }
+
+        private void OnConnectionShutdown(object sender, ShutdownEventArgs e)
+        {
+            if (_disposed) return;
+
+            _logger.LogWarning("A RabbitMQ connection is shutdown. Trying to re-connect...");
+
+            TryConnect();
+        }
+
+        public IModel CreateModel()
+        {
+            if (!IsConnected)
+            {
+                throw new InvalidOperationException("No RabbitMQ connections are available to perform this action");
+            }
+
+            return _connection.CreateModel();
+        }
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+            _disposed = true;
+            try
+            {
+                _connection.Dispose();
+            }
+            catch (Exception exception)
+            {
+                _logger.LogCritical(exception.ToString());
+            }
+        }
+    }
+}
---
+++ src/Services/Common/EventBusRabbitMQ/Events/Abstractions/IEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EventBusRabbitMQ.Events.Abstractions
+{
+    public abstract class IEvent
+    {
+        protected IEvent()
+        {
+            CreationDate = DateTime.UtcNow;
+            RequestId = Guid.NewGuid();
+        }
+
+        public Guid RequestId { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
