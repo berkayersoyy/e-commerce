d66e6d0a99d0e22eb5db301d141bc36319f1f17c
berkayersoy <berkay.ersoy.007@gmail.com>
2021-08-29 01:00:26 +0300

    Product view model added

---
+++ src/WebApp/ECommerce.WebApp.UI/ViewModels/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ECommerce.WebApp.UI.ViewModels
+{
+    public class ProductViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+    }
+}
