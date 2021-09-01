949b14fae122d7629e91ad1ca8c0f2289b4128b1
berkayersoy <berkay.ersoy.007@gmail.com>
2021-09-01 18:38:31 +0300

    New result messages added.

--- src/WebApp/ECommerce.WebApp.Core/Common/ResultConstants.cs
+++ src/WebApp/ECommerce.WebApp.Core/Common/ResultConstants.cs
@@ -4,5 +4,12 @@
     {
         public const string RecordFound = "Record found";
         public const string RecordNotFound = "Record not found";
+        public const string RecordCreateSuccessfully = "Record create successfully";
+        public const string RecordCreateNotSuccessfully = "Record create not successfully";
+        public const string RecordUpdateSuccessfully = "Record update successfully";
+        public const string RecordUpdateNotSuccessfully = "Record update not successfully";
+
+        public const string RecordDeleteSuccessfully = "Record delete successfully";
+        public const string RecordDeleteNotSuccessfully = "Record delete not successfully";
     }
 }
