2dc81912f9247e610ef34ec3b4db437cdc78be99
berkayersoy <berkay.ersoy.007@gmail.com>
2021-09-06 21:17:14 +0300

    New properties added

--- src/Services/Product/ECommerce.Product.API/Entities/Product.cs
+++ src/Services/Product/ECommerce.Product.API/Entities/Product.cs
@@ -11,9 +11,10 @@ namespace ECommerce.Product.API.Entities
         [BsonElement("Name")] 
         public string Name { get; set; }
         public string Category { get; set; }
-        public string Summary { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
         public decimal Price { get; set; }
+        public string Size { get; set; }
+        public string Color { get; set; }
     }
 }
