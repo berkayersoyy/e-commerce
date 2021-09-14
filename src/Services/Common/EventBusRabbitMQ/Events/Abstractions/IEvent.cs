1260d32009ebd9a51f2d07f2a32abe799ad7b3e8
berkayersoy <berkay.ersoy.007@gmail.com>
2021-09-14 23:48:16 +0300

    Small changes

--- src/Services/Common/EventBusRabbitMQ/Events/Abstractions/IEvent.cs
+++ src/Services/Common/EventBusRabbitMQ/Events/Abstractions/IEvent.cs
@@ -10,6 +10,12 @@ namespace EventBusRabbitMQ.Events.Abstractions
             RequestId = Guid.NewGuid();
         }
 
+        protected IEvent(Guid requestId, DateTime creationDate)
+        {
+            RequestId = requestId;
+            CreationDate = creationDate;
+        }
+
         public Guid RequestId { get; set; }
         public DateTime CreationDate { get; set; }
     }
