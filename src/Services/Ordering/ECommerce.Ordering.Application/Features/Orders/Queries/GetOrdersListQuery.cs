65ddc02bea0b538aabbe63e45551dcad56cda733
berkayersoy <berkay.ersoy.007@gmail.com>
2021-09-14 23:49:55 +0300

    Get orders list added with CQRS

---
+++ src/Services/Ordering/ECommerce.Ordering.Application/Features/Orders/Queries/GetOrdersListQuery.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using MediatR;
+
+namespace ECommerce.Ordering.Application.Features.Orders.Queries
+{
+    public class GetOrdersListQuery : IRequest<IEnumerable<OrdersVm>>
+    {
+        public string Email { get; set; }
+
+        public GetOrdersListQuery(string email)
+        {
+            Email = email;
+        }
+    }
+}
---
+++ src/Services/Ordering/ECommerce.Ordering.Application/Features/Orders/Queries/GetOrdersListQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using ECommerce.Ordering.Application.Contracts.Persistence;
+using MediatR;
+
+namespace ECommerce.Ordering.Application.Features.Orders.Queries
+{
+    public class GetOrdersListQueryHandler :IRequestHandler<GetOrdersListQuery,IEnumerable<OrdersVm>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        {
+            var orderList = await _orderRepository.GetOrdersByEmail(request.Email);
+            return _mapper.Map<IEnumerable<OrdersVm>>(orderList);
+        }
+    }
+}
