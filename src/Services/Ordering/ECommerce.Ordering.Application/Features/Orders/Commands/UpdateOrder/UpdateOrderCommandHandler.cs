ed9fbddc904496cf4928f7e18d522a45eeb49d19
berkayersoy <berkay.ersoy.007@gmail.com>
2021-09-14 23:49:41 +0300

    Update order added with CQRS

---
+++ src/Services/Ordering/ECommerce.Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using ECommerce.Ordering.Application.Contracts.Persistence;
+using ECommerce.Ordering.Application.Exceptions;
+using ECommerce.Ordering.Domain.Entities;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace ECommerce.Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandHandler:IRequestHandler<UpdateOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UpdateOrderCommandHandler> _logger;
+
+        public UpdateOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<UpdateOrderCommandHandler> logger)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToUpdate = await _orderRepository.GetByIdAsync(request.Id);
+            if (orderToUpdate==null)
+            {
+                throw new NotFoundException(nameof(Order),request.Id);
+            }
+
+            _mapper.Map(request, orderToUpdate, typeof(UpdateOrderCommand), typeof(Order));
+
+            await _orderRepository.UpdateAsync(orderToUpdate);
+
+            _logger.LogInformation($"Order {orderToUpdate.Id} is successfully updated");
+
+            return Unit.Value;
+        }
+    }
+}
