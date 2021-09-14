6e4cd558574a991d15aaf13ed370b500827c948a
berkayersoy <berkay.ersoy.007@gmail.com>
2021-09-14 23:49:30 +0300

    Delete order added with CQRS

---
+++ src/Services/Ordering/ECommerce.Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ECommerce.Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
