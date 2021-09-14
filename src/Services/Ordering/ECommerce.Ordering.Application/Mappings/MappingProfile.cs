20a5abebffff501107c26a2a719619f0669da58a
berkayersoy <berkay.ersoy.007@gmail.com>
2021-09-14 23:50:21 +0300

    Mappings added with automapper

--- src/Services/Ordering/ECommerce.Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/ECommerce.Ordering.Application/Mappings/MappingProfile.cs
@@ -1,7 +1,18 @@
-﻿namespace ECommerce.Ordering.Application.Mappings
+﻿using AutoMapper;
+using ECommerce.Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using ECommerce.Ordering.Application.Features.Orders.Commands.UpdateOrder;
+using ECommerce.Ordering.Application.Features.Orders.Queries;
+using ECommerce.Ordering.Domain.Entities;
+
+namespace ECommerce.Ordering.Application.Mappings
 {
-    public class MappingProfile
+    public class MappingProfile : Profile
     {
-        
+        public MappingProfile()
+        {
+            CreateMap<Order, OrdersVm>().ReverseMap();
+            CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+        }
     }
 }
