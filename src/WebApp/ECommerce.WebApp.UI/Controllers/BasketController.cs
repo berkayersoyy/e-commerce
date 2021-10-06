af25f6b0b4d2b80701d87e81f9939abb1b856480
berkayersoy <berkay.ersoy.007@gmail.com>
2021-10-06 19:25:00 +0300

    Basket page added

---
+++ src/WebApp/ECommerce.WebApp.UI/Controllers/BasketController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebApp.UI.Controllers
+{
+    public class BasketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
